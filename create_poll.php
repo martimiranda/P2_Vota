ee3d0e222d6eaa52918c7fa76b6d7b8fc7448234
martimiranda <martimiranda2356@gmail.com>
2024-01-25 20:27:37 +0100

    dede

--- create_poll.php
+++ create_poll.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Creación Encuesta</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="create_poll.js"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     
