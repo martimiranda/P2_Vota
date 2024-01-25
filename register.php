3ef1d105bf8e0ed40b1057e764e7abf5f08b0c1d
martimiranda <martimiranda2356@gmail.com>
2024-01-25 20:00:37 +0100

    m

--- register.php
+++ register.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="script.js"></script>
+    <script src="regsiter.js"></script>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
