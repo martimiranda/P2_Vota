d549a1250c9b189bd9c44b7886d2f981c3368624
MehdiRegg777 <96047417+MehdiRegg777@users.noreply.github.com>
2024-01-30 15:29:21 +0100

    comit

--- errores/error404.php
+++ errores/error404.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Error 404 - Not Found</title>
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body class="error404">
     <header>
