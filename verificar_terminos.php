e5f1e2d0380cc9842100916907996fac66a0a1c0
MehdiRegg777 <mregbaoui.cf@iesesteveterradas.cat>
2024-02-03 14:59:43 +0100

    s

--- verificar_terminos.php
+++ verificar_terminos.php
@@ -30,7 +30,7 @@ if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
                 <label for="acceptTerms" style="text-align: justify;">Al utilizar MARGOMI VOTOS, estás aceptando nuestros términos de privacidad y seguridad. Nos comprometemos a salvaguardar tu confidencialidad; no compartimos tus datos sin tu consentimiento. Utiliza la plataforma de manera ética y legal, respetando los derechos de propiedad intelectual. Al aceptar, también consientes en recibir comunicaciones relacionadas con la plataforma. Nos reservamos el derecho de cerrar cuentas en caso de violaciones o actividades perjudiciales. Agradecemos tu participación en MARGOMI VOTOS.</label>
                 <br>
                 <div class="container">
-                    <input class="checkbox-container2" type="checkbox" id="acceptTerms" name="acceptTerms" required>
+                    <input style="width: 0px;" class="checkbox-container2" type="checkbox" id="acceptTerms" name="acceptTerms" required>
                     <label class="checkbox-container" for="acceptTerms">Acepto las condiciones</label>
                 </div>
                 <br>
