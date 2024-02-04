324d9009994b76a1566da9ef84e5a1536c4b2f2d
MehdiRegg777 <mregbaoui.cf@iesesteveterradas.cat>
2024-02-04 14:54:18 +0100

    error

--- list_polls.php
+++ list_polls.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-...." crossorigin="anonymous" />
     <title>Listado encuestas</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="list_polls.js"></script>
+    <!-- <script src="list_polls.js"></script> -->
     <link rel="stylesheet" href="style.css?no-cache=<?php echo time(); ?>">
     <link rel="shortcut icon" href="img/vote_icon_logo.png" />
 </head>
