29e7d734ea2d5637d201e074ba1919c99f5e588a
MehdiRegg777 <96047417+MehdiRegg777@users.noreply.github.com>
2024-02-05 16:14:43 +0100

    okk

--- detailsPoll.php
+++ detailsPoll.php
@@ -121,9 +121,9 @@ if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
 
                 if ($result) {
                     $vote_count = $result['vote_count'];
-                    echo "<h1>Votos realizados para la pregunta: $vote_count</h1>";
+                    echo "<h1>Votos realizados para la encuesta: $vote_count</h1>";
                 } else {
-                    echo "<h1>No hay votos para la pregunta seleccionada</h1>";
+                    echo "<h1>No hay votos para la encuesta seleccionada</h1>";
                 }
 
 
