e5e7fdec378b568384e8113d68cc1b0cea6b081f
MehdiRegg777 <96047417+MehdiRegg777@users.noreply.github.com>
2024-02-05 16:38:21 +0100

    ok

--- detailsPoll.php
+++ detailsPoll.php
@@ -15,11 +15,8 @@ if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar sesión</title>
     <link rel="stylesheet" href="style.css?no-cache=<?php echo time(); ?>">
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <link rel="shortcut icon" href="img/vote_icon_logo.png" />
-
     <script src="https://kit.fontawesome.com/8946387bf5.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.2.0/dist/chart.umd.js"></script>
@@ -88,18 +85,18 @@ if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
                 echo  "<div id='box3'>";
 
                 $startDate = new DateTime($row["date_start"]);
-                echo "<h4>Fecha de inicio: ".$startDate->format("d/m/Y h:i")."</h4>";
+                echo "<h4 style='line-height: 1.5;'>Fecha de inicio: ".$startDate->format("d/m/Y h:i")."</h4>";
                 $endDate = new DateTime($row["date_end"]);
-                echo "<h4>Fecha de fin: ".$endDate->format("d/m/Y h:i")."</h4>";
+                echo "<h4 style='line-height: 1.5;'>Fecha de fin: ".$endDate->format("d/m/Y h:i")."</h4>";
                 echo "<br>";
 
-                echo "<h4>Estado de publicación:   ";
+                echo "<h4 style='line-height: 1.5;'>Estado de publicación:   ";
                 echo "<select id='questionVisibility'>";
                 echo "<option value='hidden' ".($row["estadoPregunta"] == "hidden" ? "selected" : "").">Oculto</option>";
                 echo "<option value='public' ".($row["estadoPregunta"] == "public" ? "selected" : "").">Público</option>";
                 echo "<option value='private' ".($row["estadoPregunta"] == "private" ? "selected" : "").">Privado</option>";
                 echo "</select></h4>";
-                echo "<h4>Estado de resultados:   ";
+                echo "<h4 style='line-height: 1.5;'>Estado de resultados:   ";
                 echo "<select id='answerVisibility'>";
                 echo "<option value='hidden' ".($row["estadoRespuesta"] == "hidden" ? "selected" : "").">Oculto</option>";
                 echo "<option value='public' ".($row["estadoRespuesta"] == "public" ? "selected" : "").">Público</option>";
