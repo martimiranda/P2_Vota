fb92653e573f99d293cb5c1cdda36f1165af8499
MehdiRegg777 <96047417+MehdiRegg777@users.noreply.github.com>
2024-02-07 17:04:28 +0100

    okk

--- sistemLog.php
+++ sistemLog.php
@@ -1,12 +1,21 @@
 <?php
-$logDirectory = "/var/www/html/P2_Vota/sistemErrorLogs/";
+$logDirectory = "sistemErrorLogs/";
+$logDirectory2 = "/var/www/html/P2_Vota/sistemErrorLogs/";
+
 $archivoLog = $logDirectory . "log_" . date("Y-m-d") . ".txt";
+$archivoLog2 = $logDirectory2 . "log_" . date("Y-m-d") . ".txt";
+
 date_default_timezone_set('Europe/Madrid');
 function registrarEvento($mensaje) {
+    global $archivoLog2;
     global $archivoLog;
-
+    
     $mensajeRegistro = "[" . date("Y-m-d H:i:s") . "] $mensaje" . PHP_EOL;
 
-    file_put_contents($archivoLog, $mensajeRegistro, FILE_APPEND | LOCK_EX);
+    try {
+        file_put_contents($archivoLog2, $mensajeRegistro, FILE_APPEND | LOCK_EX);
+    } catch (Exception $e) {
+        file_put_contents($archivoLog, $mensajeRegistro, FILE_APPEND | LOCK_EX);
+    }
 }
 ?>
